dda65cf7cb10e3288e518ec9d63590640b0596d8
fruitybits <fruitybitsgames@gmail.com>
2017-08-21 14:23:30 +0200

    Update MorseMachine.py

--- MorseMachine.py
+++ MorseMachine.py
@@ -1,5 +1,6 @@
-#This program encodes English text to Morse Code and decodes Morse Code to English text
+#Given an inital input of English or Morse, this program encodes English text to Morse Code and decodes Morse Code to English text
 #Program written by Chelsea James for ATA
+#There are 3 bugs / TODOs
 
 import sys
 import cs50
@@ -32,29 +33,26 @@ def main():
 #prompt user for input. get user input. (v2, would accept input from .txt file)
 
     
-#for now, have the user define which encode / decode function to use    
+#user defines which function to use, encode or decode. KNOWN BUG / TODO - Incomplete user input handling   
     print ("Are you entering Morse Code or Plain English?", end=" ")
-    #while True:
-
     initDefinition = input()
+    print ("Great! Please enter text:", end=" ")
+    userInput = input()
+    #while True:
     #    if ["english", "morse"] in initDefinition.tolower():
     #        break
     #    else:
     #        print ("Please enter morse or english")
-        
-    print ("Great! Please enter text:", end=" ")
-    userInput = input()
-
-        
+    
+#encode from english to morse. KNOWN BUG / TODO - Make userInput case insensitive
     if "English" in initDefinition:
-        #encode from english to morse
         for char in userInput:
             for key, val in morseEncodeDict.items():
                 if char.upper() == key:
                     print (val, end=" ")
         print ('\n')
 
-    #decode from morse to english. KNOWN BUG - Improper Spacing
+#decode from morse to english. KNOWN BUG / TODO - Improper Spacing, Make userInput case insensitive
     if "Morse" in initDefinition:
         #morseHolder = []
         morseChain = userInput.split()
